48a720a3f6e965e9ed8a7e8f95e25de0b0895091
jClancey125 <clanceyj125@gmail.com>
2023-09-02 03:10:22 +0000

    will now test for 200 response and output error message

--- secondRun/frontend/src/App.js
+++ secondRun/frontend/src/App.js
@@ -3,31 +3,45 @@ import './App.css';
 import React, { useEffect, useState, Component } from 'react';
 import axios from 'axios'
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route }
+import { BrowserRouter as Router, Routes, Route, useNavigate, Link, BrowserRouter, Redirect}
     from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+
 import Home from './pages';
 import About from './pages/about';
 import Blogs from './pages/blogs';
 import SignUp from './pages/signup';
 import Contact from './pages/contact';
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import green from "@material-ui/core/colors/green";
 
 function App() {
+  
+
   const [getMessage, setGetMessage] = useState({})
   const [isShow, setIsShow] = useState(false)
+  const useStyles = makeStyles((theme) => ({
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1
+    },
+    customColor: {
+      // or hex code, this is normal CSS background-color
+      backgroundColor: green[500]
+    },
+    customHeight: {
+      minHeight: 200
+    },
+    offset: theme.mixins.toolbar
+  }));
   
-  function displayARP()
-  {
-    if(isShow === true)
-    {
-      
-    }
-    return (
-      <div>
-        <h3>{getMessage.data}
-        
-        PLEASE WORK</h3></div>
-    )
-  }
   
   
   const testUser = {
@@ -36,47 +50,28 @@ function App() {
     imageSize: 420,
   };
 
-  useEffect(()=>{
-    axios.get('http://137.184.194.3:5000/').then(response => {
-      console.log("SUCCESS", response)
-      setGetMessage(response)
-    }).catch(error => {
-      console.log(error)
-    })
-
-  }, [])
   return (
     
     
     <div className="App">
       
       <header className="App-header">
-        <img
-          className = "testPic"
-          src = {testUser.imageUrl}
-          style={{
-            width: testUser.imageSize,
-            height: testUser.imageSize
-          }}
-        />
-
-        <TestButton />
-        <p>{displayARP}</p>
-        {displayARP}
         
-        {isShow ?
-        (
-          <h1>uhh is this working? {getMessage.data}</h1>
-        )
-      : (
-        <h1>uhh i don't think its working</h1>
-      )
-      }
 
-        <p>DAHHHHHH </p>
+        
+       
 
-        <Router>
-            <Navbar />
+        
+
+        
+      
+
+
+
+      <Router>
+          
+      <ButtonAppBar/>
+     
             <Routes>
                 <Route exact path='/' exact element={<Home />} />
                 <Route path='/about' element={<About />} />
@@ -84,9 +79,7 @@ function App() {
                 <Route path='/blogs' element={<Blogs />} />
                 <Route path='/sign-up' element={<SignUp />} />
             </Routes>
-        </Router>
-                
-            
+        </Router>         
       
       </header>
       
@@ -112,6 +105,91 @@ function App() {
       </button>
     );
   }
+
+  
+
+  function ButtonAppBar() {
+    const classes = useStyles();
+    const [example, setExample] = useState("primary");
+    const isCustomColor = example === "customColor";
+    const isCustomHeight = example === "customHeight";
+    let navigate = useNavigate(); 
+    const routeChange = () =>{ 
+    let path = `/about`; 
+    navigate(path);}
+    
+    
+
+
+    return (
+      <React.Fragment>
+        <AppBar
+          color={isCustomColor || isCustomHeight ? "primary" : example}
+          className={`${isCustomColor && classes.customColor} ${
+            isCustomHeight && classes.customHeight
+          }`}
+        >
+          <router>
+            <route>
+          
+          <Toolbar>
+            
+            <IconButton
+
+              
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              
+
+              <img style = {{width: 69, height: 69}} src={testUser.imageUrl}/>
+            
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Uhh FrontEnd Test for Automation Thingy
+            </Typography>
+            <Link to="/sign-up">
+            <IconButton color="inherit" onClick={() => setExample("primary")}>
+              Home
+              
+            </IconButton>
+            </Link>
+            <Link to="/">
+            <IconButton color="inherit" onClick={() => setExample("primary")}>
+              Cisco
+            </IconButton>
+            </Link>
+            <IconButton color="inherit" onClick= {routeChange}>
+              
+              Else
+            </IconButton>
+            <IconButton color="inherit" onClick={() => setExample("transparent")}>
+              4
+            </IconButton>
+            <Link to="/blogs">
+            <IconButton color="inherit" onClick={() => setExample("customColor")}>
+              5
+            </IconButton>
+            </Link>
+            <IconButton
+              color="inherit"
+              onClick={() => setExample("customHeight")}
+            >
+              6
+            </IconButton>
+          </Toolbar>
+          </route>
+          </router> 
+            
+        </AppBar>
+        <Toolbar />    
+      </React.Fragment>
+    );
+  }
+  
+
 
   
 
--- secondRun/frontend/src/pages/index.js
+++ secondRun/frontend/src/pages/index.js
@@ -1,11 +1,117 @@
-import React from 'react';
+import React, { useEffect, useState, Component } from 'react';
+import axios from 'axios'
+import { BrowserRouter as Router, Routes, Route }
+    from 'react-router-dom';
+
  
 const Home = () => {
+    const [getMessage, setGetMessage] = useState({})
+    const [getOldMessage, setOldGMessage] = useState({})
+    const [isShow, setIsShow] = useState(false)
+    var newTest 
+    const [getInt, setInt] = useState({})
+    
+    
+    
+    
+
+
+    
+    
+
+    useEffect(()=>{
+        axios.get('http://137.184.194.3:5000/').then(response => {
+
+          if(response.status === 200){
+            console.log("SUCCESS", response)
+            setGetMessage(response)
+            setOldGMessage(response)
+          }
+          else{
+            if(setOldGMessage){
+              setGetMessage(setOldGMessage)
+            }
+            
+          }
+
+
+
+
+          
+        }).catch(error => {
+          console.log(error)
+          
+            
+          
+        })
+    
+      }, [])
+      //testJson()
     return (
-        <div>
-            <h1>Test Homepage for assholes</h1>
+      <div>
+        <Heading/>
+        <TestButton message={getMessage.data} buttonMessage= {'show ARP'}/>
         </div>
+        
     );
+
+    function Heading() {
+        
+          return ( <h1>
+              Cisco IOS integration PoC 
+              requires backend to be running
+          </h1>
+          );
+        
+
+      
+    }
+
+    
+
+    function testJson(){
+
+      const newTests = getMessage.data
+      newTest = newTests
+
+
+    }
+
+
+    function TestButton({message, buttonMessage}){
+        const [count, setCount] = useState(0);
+        //testJson()
+        function handleClick(){
+          setIsShow(true)
+          //setCount(count +1);
+        }
+        if (message === undefined || message === null)
+        {
+          message = ("please wait bruh")
+        }
+        
+    
+        return (
+          //<button>I'm just a button in a human world</button>
+          <div><button onClick = {handleClick}>
+            {(buttonMessage)} is {String(isShow)}
+    
+    
+          </button>
+
+          {isShow ? 
+        (
+          
+          <p>{message}</p>
+        )
+      : (
+        <p>uhh i don't think its working</p>
+      )
+      }
+
+          </div>
+        );
+    }
 };
  
 export default Home;
